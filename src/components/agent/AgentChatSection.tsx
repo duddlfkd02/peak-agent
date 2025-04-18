6efb86f09dd2f8829bbd754dd831542c07a6fdb2
gimhayeong <duddlfkd02@gmail.com>
2025-04-18 14:48:37 +0900

    add : 문자 추가 시 자동 스크롤 / update : 리드 탐색 api 연결 확인 완료

--- src/components/agent/AgentChatSection.tsx
+++ src/components/agent/AgentChatSection.tsx
@@ -3,13 +3,22 @@
 import { Textarea } from "@/components/ui/textarea";
 import { Button } from "@/components/ui/button";
 import { Send } from "lucide-react";
-import { useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import { useSearchStore } from "@/store/useSearchStore";
 import { pdfSummary } from "@/types/pdfSummary";
 
 export default function AgentChatSection() {
   const [message, setMessage] = useState("");
   const [messages, setMessages] = useState<string[]>([]);
+  const [leads, setLeads] = useState<any[]>([]);
+
+  const messageEndRef = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    if (messageEndRef.current) {
+      messageEndRef.current.scrollTop = messageEndRef.current.scrollHeight;
+    }
+  }, [messages]);
 
   const { pdfSummary } = useSearchStore();
   console.log("zustand pdfSummary", pdfSummary);
@@ -52,6 +61,30 @@ export default function AgentChatSection() {
     return introParts.join("\n\n").trim();
   };
 
+  const BASE_URL = process.env.NEXT_PUBLIC_API_BASE_URL;
+
+  const handleFindLeads = async () => {
+    // if (!pdfSummary?.company_id) {
+    //   alert("company_id가 없습니다");
+    //   return;
+    // -> companu_id가 pdfSummary에 없어서 추가 필요
+
+    try {
+      const response = await fetch(`${BASE_URL}/api/scout/find-leads/`, {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({ company_id: 94 })
+      });
+
+      if (!response.ok) throw new Error("리드 탐색에 실패했습니다.");
+      const data = await response.json();
+      console.log("data 결과", data);
+      setLeads(data.leads);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
   const handleSubmit = () => {
     if (!message.trim()) return;
     setMessages((prev) => [...prev, message]);
@@ -65,34 +98,53 @@ export default function AgentChatSection() {
     }
   };
   return (
-    <div className="flex min-h-[calc(100vh-4rem)] flex-col rounded-lg bg-foreground p-4">
+    <div className="flex h-[calc(100vh-4rem)] flex-col rounded-lg bg-foreground p-4">
       <h2 className="mb-4 w-full text-left md:text-lg">Agent</h2>
 
-      <div className="flex flex-1 flex-col justify-end">
-        <div className="mb-4 overflow-y-auto rounded p-4 text-sm">
-          {pdfSummary ? <div className="text-white">{generateCompanyIntro(pdfSummary)}</div> : "요약 정보가 없습니다."}
+      <div ref={messageEndRef} className="flex-1 space-y-4 overflow-y-auto rounded p-4">
+        {pdfSummary ? <div className="text-white">{generateCompanyIntro(pdfSummary)}</div> : "요약 정보가 없습니다."}
+        {pdfSummary && (
+          <Button className="mt-4 w-fit border bg-transparent text-white" onClick={handleFindLeads}>
+            리드 탐색하기
+          </Button>
+        )}
 
+        <div>
+          {leads.length > 0 && (
+            <div className="mt-6">
+              <h3 className="bold mb-2 text-white">추천 리드</h3>
+              <ul className="space-y-2">
+                {leads.map((lead, index) => (
+                  <li key={index} className="rounded bg-darkgray p-3 text-white">
+                    <p>회사명: {lead.company}</p>
+                    <p>산업군: {lead.industry}</p>
+                    <p>홈페이지: {lead.homepage}</p>
+                  </li>
+                ))}
+              </ul>
+            </div>
+          )}
           {messages.map((msg, index) => (
             <div key={index} className="rounded px-4 py-2 text-right text-white">
               {msg}
             </div>
           ))}
         </div>
+      </div>
 
-        {/* 채팅 입력창 영역 */}
-        <div className="relative">
-          <Textarea
-            value={message}
-            onChange={(e) => setMessage(e.target.value)}
-            onKeyUp={handleKeyDown}
-            placeholder="궁금한 내용을 입력해보세요."
-            className="min-h-[120px] w-full resize-none pr-24 text-sm text-muted placeholder:text-muted sm:text-base"
-          />
-          <div className="absolute bottom-3 right-3 flex gap-2">
-            <Button size="icon" variant="ghost" className="bg-primary" onClick={handleSubmit}>
-              <Send className="h-4 w-4" />
-            </Button>
-          </div>
+      {/* 채팅 입력창 영역 */}
+      <div className="relative mt-4">
+        <Textarea
+          value={message}
+          onChange={(e) => setMessage(e.target.value)}
+          onKeyUp={handleKeyDown}
+          placeholder="궁금한 내용을 입력해보세요."
+          className="min-h-[120px] w-full resize-none pr-24 text-sm text-muted placeholder:text-muted sm:text-base"
+        />
+        <div className="absolute bottom-3 right-3 flex gap-2">
+          <Button size="icon" variant="ghost" className="bg-primary" onClick={handleSubmit}>
+            <Send className="h-4 w-4" />
+          </Button>
         </div>
       </div>
     </div>
